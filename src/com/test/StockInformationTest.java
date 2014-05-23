ca3e677348fc565cc15924cba708339ac246b8d2
Jonathan McCann <jonathan.mccann@liferay.com>
2014-05-23 15:35:43 -0700

    Add test for getters and setters

--- src/com/test/StockInformationTest.java
+++ src/com/test/StockInformationTest.java
@@ -33,4 +33,29 @@ public class StockInformationTest {
 			System.out.println();
 		}
 	}
+
+	@Test
+	public void testGettersAndSetters() {
+		Stock stock = new Stock();
+
+		stock.setChange("+0.56");
+		stock.setDayRange("1.52 - 2.56");
+		stock.setDividendPayDate("Feb 28 2014");
+		stock.setExDividendDate("March 31 2014");
+		stock.setLastTrade("500.87");
+		stock.setName("Apple");
+		stock.setOpen("500.25");
+		stock.setPercentChange("+0.23%");
+		stock.setTickerSymbol("AAPL");
+
+		assert(stock.getChange().equals("+0.56"));
+		assert(stock.getDayRange().equals("1.52 - 2.56"));
+		assert(stock.getDividendPayDate().equals("Feb 28 2014"));
+		assert(stock.getExDividendDate().equals("March 31 2014"));
+		assert(stock.getLastTrade().equals("500.87"));
+		assert(stock.getName().equals("Apple"));
+		assert(stock.getOpen().equals("500.25"));
+		assert(stock.getPercentChange().equals("+0.23%"));
+		assert(stock.getTicketSymbol().equals("AAPL"));
+	}
 }
