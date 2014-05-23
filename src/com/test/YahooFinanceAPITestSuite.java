562dc9c1380c6f873e1b4088770e8fa69e6338ac
Jonathan McCann <jonathan.mccann@liferay.com>
2014-05-23 15:26:29 -0700

    Remove MailTest since simply running the program will test this

--- src/com/test/YahooFinanceAPITestSuite.java
+++ src/com/test/YahooFinanceAPITestSuite.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({MailTest.class, StockInformationTest.class})
+@Suite.SuiteClasses({StockInformationTest.class})
 public class YahooFinanceAPITestSuite {
 
 	@BeforeClass
